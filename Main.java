7926ebfc8b9498b22be898d65dcd013cd028ef80
aadicodes2024 <aadicodes2024@gmail.com>
2024-06-04 00:09:45 +0530

    Another device testing

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("ABC");
+        System.out.println("Test commit from another device");
     }
 }
